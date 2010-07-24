f9aa6755bfa860b2cd9e23e30b108c83440be12f
Fernando Ferreira <fferreira@icsystems.com.br>
2010-07-24 18:18:08 -0300

    Versao Inicial

---
+++ cgi-bin/submitForm.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+print 'Content-type: text/html'
+print
+
+import cgi
+import cgitb
+import sys
+import string
+import numpy as np
+cgitb.enable()
+
+from xml.dom import minidom
+from Cheetah.Template import Template
+from unicodedata import normalize
+
+def normalizeString(txt, codif='utf-8'):
+	txt = normalize('NFKD', txt.decode(codif)).encode('ASCII','ignore')
+	return txt.lower()
+
+def createXML(keys, dictValues):
+	xmlStr = '<?xml version="1.0" encoding="utf-8"?>'
+	xmlStr += '<documento>'
+	xmlStr += '<paciente>'
+	xmlStr += '<exames>'
+	for k in keys:
+		if k != 'score':
+			xmlStr += '<%s>%s</%s>'%(k,dictValues[k],k)
+	xmlStr += '</exames>'
+	xmlStr += '</paciente>'
+	xmlStr += '</documento>'
+	return xmlStr
+
+def Main():
+	form = cgi.FieldStorage()
+	#Build dict
+	values = {}
+	for k in form:
+		v = form[k]
+		if type(v) == list:
+			values[k] = []
+			for el in v:
+				values[k].append(el.value)
+		else:
+			values[k] = v.value
+	keys = [k for k in form]
+	xmlStr = createXML(keys,values)
+	domObj = minidom.parseString(xmlStr)
+	xmlContent = domObj.toxml(encoding='utf-8')
+	#make output
+	templateDef = u"""
+	<html>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<head>
+			<title> Formulário Exames </title>
+			<style type='text/css'>
+			<!--
+				@import url(../css/submitForm_style.css);
+			-->
+			</style>
+		</head>
+		<body>
+			<div id='header'>
+				$score
+			</div>
+			<div id='content'>
+				<div id='leftpanel'>
+					<table>
+						<thead>
+						<tr>
+							<th colspan='2'> Dados </th>
+						</tr>
+						</thead>
+						<tbody>
+	"""
+	for k in form:
+		templateDef += """
+						<tr>
+							<th>%s </th>
+							<td>$%s</td>
+						<tr>
+		"""%(k,k,)
+	templateDef+="""
+						</tbody>
+					</table>
+				</div>
+				<div id='center'>
+					<h2>Para CONFIRMAR e REGISTRAR o paciente:</h2>
+					<form action='submitXML.py' method='POST'>
+						<input type='hidden' value='$xmlPaciente' name='xmlPaciente' />
+						<input type='submit' value='Registrar'/>
+					</form>
+				</div>
+			</div>
+		</body>
+	</html>
+	"""
+	values['xmlPaciente'] = xmlContent
+	templateDef = templateDef.encode('utf-8', 'xmlcharrefreplace')
+	t = Template(templateDef, searchList=[values])
+	print t
+
+if __name__ == '__main__':
+	Main()
